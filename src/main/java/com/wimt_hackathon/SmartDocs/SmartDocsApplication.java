0edd48e1ce05ab9b30fcbbe5c3b7c166253d3283
Abhishek Narayan Singh <abhisheknarayan354@gmail.com>
2024-11-07 19:51:44 +0530

    Project Initialization

---
+++ src/main/java/com/wimt_hackathon/SmartDocs/SmartDocsApplication.java
@@ -0,0 +1,13 @@
+package com.wimt_hackathon.SmartDocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartDocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartDocsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wimt_hackathon/SmartDocs/SmartDocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wimt_hackathon.SmartDocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartDocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
